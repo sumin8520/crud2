c559a16ba6e645af786d553eff6b9ad1cee1e5f2
sumin <jbsumin8424@gmail.com>
2023-11-14 12:38:36 +0900

    1st

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import TopicsList from '@/components/TopicsList'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold"> WebDev Topics</h1>
+      <p className="mb-4">Learning example of MongoDB CRUD</p>
+      <TopicsList />
+    </>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center bg-red-900 px-8 py-3">
+      <Link className="text-white text-lg font-bold" href="/">
+        MongoDB CRUD
+      </Link>
+      <Link
+        className="bg-yellow-200 text-gray-500 text-lg font-bold px-4 py-2 rounded-md"
+        href="/addTopic"
+      >
+        Add Topic
+      </Link>
+    </nav>
+  )
+}
